3b06a7e20349a7bc309ebbd46c8672ff0b4a474d
spnbeatz <111923195+spnbeatz@users.noreply.github.com>
2024-07-16 18:58:53 +0200

    First steps in creating this project

---
+++ app/Room.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ReactNode } from "react";
+
+import { 
+    LiveblocksProvider,
+    RoomProvider,
+    ClientSideSuspense
+} from "@liveblocks/react/suspense";
+
+
+export function Room({children}: {children: ReactNode}) {
+    return (
+        <LiveblocksProvider publicApiKey={process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_KEY!}>
+            <RoomProvider id="my-room">
+                <ClientSideSuspense fallback={<div>Loading...</div>}>
+                    {children}
+                </ClientSideSuspense>
+            </RoomProvider>
+        </LiveblocksProvider>
+    )
+}
